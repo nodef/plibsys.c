fa544586cb28267fa338dfdeb313d430b80f9a65
Subhajit Sahu <wolfram77@gmail.com>
2025-12-28 23:29:05 +0530

    :bug: with cmake free plibsysconfig

---
+++ plibsys.h
@@ -0,0 +1,259 @@
+#pragma once
+#include "plibsys/plibsys.h"
+
+
+#ifdef PLIBSYS_IMPLEMENTATION
+
+/* Include platform-independent core implementations */
+#include "plibsys/perror.c"
+#include "plibsys/pfile.c"
+#include "plibsys/phashtable.c"
+#include "plibsys/pinifile.c"
+#include "plibsys/pipc.c"
+#include "plibsys/plist.c"
+#include "plibsys/pmain.c"
+#include "plibsys/pmem.c"
+#include "plibsys/pprocess.c"
+#include "plibsys/pstring.c"
+
+/* Include all cryptographic hash implementations (platform-independent) */
+#include "plibsys/pcryptohash-gost3411.c"
+#include "plibsys/pcryptohash-md5.c"
+#include "plibsys/pcryptohash-sha1.c"
+#include "plibsys/pcryptohash-sha2-256.c"
+#include "plibsys/pcryptohash-sha2-512.c"
+#include "plibsys/pcryptohash-sha3.c"
+#include "plibsys/pcryptohash.c"
+
+/* Include all tree implementations (platform-independent) */
+#include "plibsys/ptree-avl.c"
+#include "plibsys/ptree-bst.c"
+#include "plibsys/ptree-rb.c"
+#include "plibsys/ptree.c"
+
+/* Platform-specific: Atomic operations */
+#if defined(P_CC_MSVC) || defined(P_CC_BORLAND)
+#include "plibsys/patomic-win.c"
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L) &&            \
+    !defined(P_CC_PGI)
+/* C11 atomics */
+#include "plibsys/patomic-c11.c"
+#elif (defined(P_CC_GNU) &&                                                    \
+    (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1))) ||               \
+    (defined(P_CC_INTEL) && __INTEL_COMPILER >= 1110) ||                       \
+    (defined(P_CC_CLANG) && __clang_major__ >= 3)
+/* GCC/Clang __sync builtins */
+#include "plibsys/patomic-sync.c"
+#elif defined(P_CC_DEC) && defined(__alpha)
+/* DEC Alpha compiler */
+#include "plibsys/patomic-decc.c"
+#else
+/* Simulation fallback */
+#include "plibsys/patomic-sim.c"
+#endif
+
+/* Platform-specific: Spinlock */
+#if defined(P_CC_MSVC) || defined(P_CC_BORLAND)
+#include "plibsys/pspinlock-win.c"
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L) &&            \
+    !defined(P_CC_PGI)
+/* C11 atomics */
+#include "plibsys/pspinlock-c11.c"
+#elif (defined(P_CC_GNU) &&                                                    \
+    (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1))) ||               \
+    (defined(P_CC_INTEL) && __INTEL_COMPILER >= 1110) ||                       \
+    (defined(P_CC_CLANG) && __clang_major__ >= 3)
+/* GCC/Clang __sync builtins */
+#include "plibsys/pspinlock-sync.c"
+#elif defined(P_CC_DEC) && defined(__alpha)
+/* DEC Alpha compiler */
+#include "plibsys/pspinlock-decc.c"
+#else
+/* Simulation fallback */
+#include "plibsys/pspinlock-sim.c"
+#endif
+
+/* Platform-specific: Mutex */
+#if defined(P_OS_WIN)
+#include "plibsys/pmutex-win.c"
+#elif defined(P_OS_BEOS)
+#include "plibsys/pmutex-beos.c"
+#elif defined(P_OS_AMIGA)
+#include "plibsys/pmutex-amiga.c"
+#elif defined(P_OS_ATHEOS)
+#include "plibsys/pmutex-atheos.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/pmutex-os2.c"
+#elif defined(P_OS_SOLARIS)
+#include "plibsys/pmutex-solaris.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+#include "plibsys/pmutex-posix.c"
+#else
+#include "plibsys/pmutex-none.c"
+#endif
+
+/* Platform-specific: Condition Variable */
+#if defined(P_OS_WIN)
+#include "plibsys/pcondvariable-win.c"
+#elif defined(P_OS_BEOS)
+#include "plibsys/pcondvariable-beos.c"
+#elif defined(P_OS_AMIGA)
+#include "plibsys/pcondvariable-amiga.c"
+#elif defined(P_OS_ATHEOS)
+#include "plibsys/pcondvariable-atheos.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/pcondvariable-os2.c"
+#elif defined(P_OS_SOLARIS)
+#include "plibsys/pcondvariable-solaris.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+#include "plibsys/pcondvariable-posix.c"
+#else
+#include "plibsys/pcondvariable-none.c"
+#endif
+
+/* Platform-specific: Read-Write Lock */
+#if defined(P_OS_WIN)
+#include "plibsys/prwlock-win.c"
+#elif defined(P_OS_SOLARIS)
+#include "plibsys/prwlock-solaris.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+#include "plibsys/prwlock-posix.c"
+#else
+/* General implementation using mutex + condvar */
+#include "plibsys/prwlock-general.c"
+#endif
+
+/* Platform-specific: Thread */
+#if defined(P_OS_WIN)
+#include "plibsys/puthread-win.c"
+#elif defined(P_OS_BEOS)
+#include "plibsys/puthread-beos.c"
+#elif defined(P_OS_AMIGA)
+#include "plibsys/puthread-amiga.c"
+#elif defined(P_OS_ATHEOS)
+#include "plibsys/puthread-atheos.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/puthread-os2.c"
+#elif defined(P_OS_SOLARIS)
+#include "plibsys/puthread-solaris.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+#include "plibsys/puthread-posix.c"
+#else
+#include "plibsys/puthread-none.c"
+#endif
+
+/* Always include the main thread wrapper */
+#include "plibsys/puthread.c"
+
+/* Platform-specific: Semaphore */
+#if defined(P_OS_WIN)
+#include "plibsys/psemaphore-win.c"
+#elif defined(P_OS_AMIGA)
+#include "plibsys/psemaphore-amiga.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/psemaphore-os2.c"
+#elif defined(P_OS_SOLARIS) || defined(P_OS_IRIX) || defined(P_OS_TRU64) ||    \
+    defined(P_OS_UNIXWARE)
+/* System V semaphores */
+#include "plibsys/psemaphore-sysv.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+/* POSIX semaphores */
+#include "plibsys/psemaphore-posix.c"
+#else
+#include "plibsys/psemaphore-none.c"
+#endif
+
+/* Platform-specific: Shared Memory */
+#if defined(P_OS_WIN)
+#include "plibsys/pshm-win.c"
+#elif defined(P_OS_AMIGA)
+#include "plibsys/pshm-amiga.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/pshm-os2.c"
+#elif defined(P_OS_SOLARIS) || defined(P_OS_IRIX) || defined(P_OS_TRU64) ||    \
+    defined(P_OS_UNIXWARE)
+/* System V shared memory */
+#include "plibsys/pshm-sysv.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+/* POSIX shared memory */
+#include "plibsys/pshm-posix.c"
+#else
+#include "plibsys/pshm-none.c"
+#endif
+
+/* Shared memory buffer (platform-independent wrapper) */
+#include "plibsys/pshmbuffer.c"
+
+/* Platform-specific: Directory operations */
+#if defined(P_OS_WIN)
+#include "plibsys/pdir-win.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/pdir-os2.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+#include "plibsys/pdir-posix.c"
+#else
+#include "plibsys/pdir-none.c"
+#endif
+
+/* Directory wrapper (platform-independent) */
+#include "plibsys/pdir.c"
+
+/* Platform-specific: Library Loader */
+#if defined(P_OS_WIN)
+#include "plibsys/plibraryloader-win.c"
+#elif defined(P_OS_BEOS)
+#include "plibsys/plibraryloader-beos.c"
+#elif defined(P_OS_AMIGA)
+#include "plibsys/plibraryloader-amiga.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/plibraryloader-os2.c"
+#elif defined(P_OS_HPUX)
+/* HP-UX uses shl_load */
+#include "plibsys/plibraryloader-shl.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+/* POSIX dlopen */
+#include "plibsys/plibraryloader-posix.c"
+#else
+#include "plibsys/plibraryloader-none.c"
+#endif
+
+/* Platform-specific: Time Profiler */
+#if defined(P_OS_WIN)
+#include "plibsys/ptimeprofiler-win.c"
+#elif defined(P_OS_BEOS)
+#include "plibsys/ptimeprofiler-beos.c"
+#elif defined(P_OS_AMIGA)
+#include "plibsys/ptimeprofiler-amiga.c"
+#elif defined(P_OS_OS2)
+#include "plibsys/ptimeprofiler-os2.c"
+#elif defined(P_OS_MAC) || defined(P_OS_DARWIN)
+/* Mach absolute time */
+#include "plibsys/ptimeprofiler-mach.c"
+#elif defined(P_OS_SOLARIS)
+/* Solaris gethrtime() */
+#include "plibsys/ptimeprofiler-solaris.c"
+#elif defined(P_OS_UNIX)
+/* POSIX clock_gettime() */
+#include "plibsys/ptimeprofiler-posix.c"
+#else
+/* Generic gettimeofday() fallback */
+#include "plibsys/ptimeprofiler-generic.c"
+#endif
+
+/* Time profiler wrapper (platform-independent) */
+#include "plibsys/ptimeprofiler.c"
+
+/* Platform-specific: System close-on-exec */
+#if defined(P_OS_WIN)
+#include "plibsys/psysclose-win.c"
+#elif defined(P_OS_DARWIN) || defined(P_OS_MAC)
+#include "plibsys/psysclose-darwin.c"
+#elif defined(P_OS_UNIX) || defined(P_OS_CYGWIN) || defined(P_OS_MSYS)
+#include "plibsys/psysclose-unix.c"
+#endif
+
+/* Network sockets (platform-independent, but uses platform headers) */
+#include "plibsys/psocket.c"
+#include "plibsys/psocketaddress.c"
+
+#endif
--- plibsys/plibsysconfig.h
+++ plibsys/plibsysconfig.h
@@ -1,74 +1,228 @@
 #ifndef PLIBSYS_HEADER_PLIBSYSCONFIG_H
 #define PLIBSYS_HEADER_PLIBSYSCONFIG_H
 
-#define PLIBSYS_VERSION_MAJOR @PLIBSYS_VERSION_MAJOR@
-#define PLIBSYS_VERSION_MINOR @PLIBSYS_VERSION_MINOR@
-#define PLIBSYS_VERSION_PATCH @PLIBSYS_VERSION_PATCH@
-#define PLIBSYS_VERSION_STR "@PLIBSYS_VERSION@"
-#define PLIBSYS_VERSION @PLIBSYS_VERSION_NUM@
+/* Version information - set to a reasonable default */
+#define PLIBSYS_VERSION_MAJOR 0
+#define PLIBSYS_VERSION_MINOR 0
+#define PLIBSYS_VERSION_PATCH 5
+#define PLIBSYS_VERSION_STR "0.0.5"
+#define PLIBSYS_VERSION                                                        \
+  ((PLIBSYS_VERSION_MAJOR << 16) | (PLIBSYS_VERSION_MINOR << 8) |              \
+   PLIBSYS_VERSION_PATCH)
 
-#cmakedefine PLIBSYS_NEED_WINDOWS_H
-#cmakedefine PLIBSYS_NEED_FLOAT_H
-#cmakedefine PLIBSYS_NEED_LIMITS_H
-#cmakedefine PLIBSYS_NEED_VALUES_H
-#cmakedefine PLIBSYS_NEED_PTHREAD_NP_H
-#cmakedefine PLIBSYS_IS_BIGENDIAN
-#cmakedefine PLIBSYS_SIZEOF_SAFAMILY_T @PLIBSYS_SIZEOF_SAFAMILY_T@
-#cmakedefine PLIBSYS_VA_COPY @PLIBSYS_VA_COPY@
+/* Detect Windows platform */
+#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) ||                \
+    defined(__WINDOWS__)
+#define PLIBSYS_NEED_WINDOWS_H
+#endif
 
-#define PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT2(ver)    ver##0000
-#define PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT(ver)     PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT2(ver)
+/* Always need float.h and limits.h for standard constants */
+#define PLIBSYS_NEED_FLOAT_H
+#define PLIBSYS_NEED_LIMITS_H
+
+/* Detect if we need values.h (mostly UNIX systems, but not all have it) */
+#if defined(__unix__) || defined(__unix) || defined(unix)
+#if !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__NetBSD__) &&    \
+    !defined(__OpenBSD__) && !defined(__DragonFly__)
+/* #  define PLIBSYS_NEED_VALUES_H */
+#endif
+#endif
+
+/* Detect if we need pthread_np.h (FreeBSD, OpenBSD, NetBSD) */
+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) ||     \
+    defined(__DragonFly__)
+#define PLIBSYS_NEED_PTHREAD_NP_H
+#endif
+
+/* Detect endianness */
+#if defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) &&                \
+    __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define PLIBSYS_IS_BIGENDIAN
+#elif defined(__BYTE_ORDER__) && defined(__ORDER_LITTLE_ENDIAN__) &&           \
+    __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+/* Little endian - no need to define anything */
+#elif defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) || \
+    defined(__AARCH64EB__) || defined(_MIPSEB) || defined(__MIPSEB) ||         \
+    defined(__MIPSEB__)
+#define PLIBSYS_IS_BIGENDIAN
+#elif defined(__sparc) || defined(__sparc__) || defined(__hppa) ||             \
+    defined(__hppa__) || defined(_POWER) || defined(__powerpc__) ||            \
+    defined(__ppc__) || defined(__PPC__)
+#if !defined(__LITTLE_ENDIAN__)
+#define PLIBSYS_IS_BIGENDIAN
+#endif
+#elif defined(__s390__) || defined(__s390x__) || defined(__zarch__)
+#define PLIBSYS_IS_BIGENDIAN
+#endif
+
+/* Detect sa_family_t size (mostly 2 bytes on all platforms) */
+#define PLIBSYS_SIZEOF_SAFAMILY_T 2
+
+/* Detect va_copy - C99 standard */
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+#define PLIBSYS_VA_COPY va_copy
+#elif defined(__GNUC__)
+#define PLIBSYS_VA_COPY __va_copy
+#else
+#define PLIBSYS_VA_COPY(dest, src) ((dest) = (src))
+#endif
+
+#define PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT2(ver) ver##0000
+#define PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT(ver)                            \
+  PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT2(ver)
 
 #ifdef PLIBSYS_NEED_WINDOWS_H
-#  ifndef _WIN32_WINNT
-#    define _WIN32_WINNT 0x501
-#  endif
-#  ifndef NTDDI_VERSION
-#    define NTDDI_VERSION PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT(_WIN32_WINNT)
-#  endif
-#  include <winsock2.h>
-#  include <ws2tcpip.h>
-#  include <windows.h>
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x501
+#endif
+#ifndef NTDDI_VERSION
+#define NTDDI_VERSION PLIBSYS_NTDDI_VERSION_FROM_WIN32_WINNT(_WIN32_WINNT)
+#endif
+// Include winsock2.h before windows.h to avoid redefinition of types
+#include <winsock2.h>
+#include <ws2tcpip.h>
+// Now include windows.h
+#include <windows.h>
 #endif
 
 #include <pmacros.h>
 
 #ifdef PLIBSYS_NEED_FLOAT_H
-#  include <float.h>
+#include <float.h>
 #endif
 
 #ifdef PLIBSYS_NEED_LIMITS_H
-#  include <limits.h>
+#include <limits.h>
 #endif
 
 #ifdef PLIBSYS_NEED_VALUES_H
-#  include <values.h>
+#include <values.h>
 #endif
 
 P_BEGIN_DECLS
 
-#define P_MINFLOAT    @PLIBSYS_FLOAT_MIN@
-#define P_MAXFLOAT    @PLIBSYS_FLOAT_MAX@
-#define P_MINDOUBLE   @PLIBSYS_DOUBLE_MIN@
-#define P_MAXDOUBLE   @PLIBSYS_DOUBLE_MAX@
-#define P_MINSHORT    @PLIBSYS_SHORT_MIN@
-#define P_MAXSHORT    @PLIBSYS_SHORT_MAX@
-#define P_MAXUSHORT   @PLIBSYS_USHORT_MAX@
-#define P_MININT      @PLIBSYS_INT_MIN@
-#define P_MAXINT      @PLIBSYS_INT_MAX@
-#define P_MAXUINT     @PLIBSYS_UINT_MAX@
-#define P_MINLONG     @PLIBSYS_LONG_MIN@
-#define P_MAXLONG     @PLIBSYS_LONG_MAX@
-#define P_MAXULONG    @PLIBSYS_ULONG_MAX@
-
-@PLIBSYS_SIZEOF_VOID_P_CODE@
-@PLIBSYS_SIZEOF_SIZE_T_CODE@
-@PLIBSYS_SIZEOF_LONG_CODE@
-
-#ifdef PLIBSYS_IS_BIGENDIAN
-#  define P_BYTE_ORDER P_BIG_ENDIAN
+/* Define min/max constants using standard library headers */
+#ifdef FLT_MIN
+#define P_MINFLOAT FLT_MIN
 #else
-#  define P_BYTE_ORDER P_LITTLE_ENDIAN
+#define P_MINFLOAT ((float)1.17549435e-38F)
+#endif
+
+#ifdef FLT_MAX
+#define P_MAXFLOAT FLT_MAX
+#else
+#define P_MAXFLOAT ((float)3.40282347e+38F)
+#endif
+
+#ifdef DBL_MIN
+#define P_MINDOUBLE DBL_MIN
+#else
+#define P_MINDOUBLE ((double)2.2250738585072014e-308)
+#endif
+
+#ifdef DBL_MAX
+#define P_MAXDOUBLE DBL_MAX
+#else
+#define P_MAXDOUBLE ((double)1.7976931348623157e+308)
+#endif
+
+#ifdef SHRT_MIN
+#define P_MINSHORT SHRT_MIN
+#else
+#define P_MINSHORT ((short)-32768)
+#endif
+
+#ifdef SHRT_MAX
+#define P_MAXSHORT SHRT_MAX
+#else
+#define P_MAXSHORT ((short)32767)
+#endif
+
+#ifdef USHRT_MAX
+#define P_MAXUSHORT USHRT_MAX
+#else
+#define P_MAXUSHORT ((unsigned short)65535)
+#endif
+
+#ifdef INT_MIN
+#define P_MININT INT_MIN
+#else
+#define P_MININT ((int)-2147483648)
+#endif
+
+#ifdef INT_MAX
+#define P_MAXINT INT_MAX
+#else
+#define P_MAXINT ((int)2147483647)
+#endif
+
+#ifdef UINT_MAX
+#define P_MAXUINT UINT_MAX
+#else
+#define P_MAXUINT ((unsigned int)4294967295U)
+#endif
+
+#ifdef LONG_MIN
+#define P_MINLONG LONG_MIN
+#else
+#define P_MINLONG ((long)-2147483648L)
+#endif
+
+#ifdef LONG_MAX
+#define P_MAXLONG LONG_MAX
+#else
+#define P_MAXLONG ((long)2147483647L)
+#endif
+
+#ifdef ULONG_MAX
+#define P_MAXULONG ULONG_MAX
+#else
+#define P_MAXULONG ((unsigned long)4294967295UL)
+#endif
+
+/* Detect pointer size */
+#if defined(__LP64__) || defined(_LP64) || defined(__64BIT__) ||               \
+    defined(_WIN64) || defined(__x86_64__) || defined(__amd64__) ||            \
+    defined(__aarch64__) || defined(__ia64__) || defined(__powerpc64__) ||     \
+    defined(__s390x__) || defined(__sparc64__) || defined(__arch64__) ||       \
+    defined(_M_X64) || defined(_M_AMD64) || defined(_M_IA64) ||                \
+    defined(_M_ARM64)
+#define PLIBSYS_SIZEOF_VOID_P 8
+#else
+#define PLIBSYS_SIZEOF_VOID_P 4
+#endif
+
+/* Detect size_t size (usually same as pointer size) */
+#if PLIBSYS_SIZEOF_VOID_P == 8
+#define PLIBSYS_SIZEOF_SIZE_T 8
+#else
+#define PLIBSYS_SIZEOF_SIZE_T 4
+#endif
+
+/* Detect long size */
+#if defined(_WIN64) || defined(_WIN32)
+/* On Windows, long is always 4 bytes even on 64-bit */
+#define PLIBSYS_SIZEOF_LONG 4
+#elif defined(__LP64__) || defined(_LP64)
+/* LP64 model: long and pointers are 64-bit */
+#define PLIBSYS_SIZEOF_LONG 8
+#elif defined(__ILP32__)
+/* ILP32 model: int, long, and pointers are 32-bit */
+#define PLIBSYS_SIZEOF_LONG 4
+#else
+/* Default assumption based on pointer size for non-Windows */
+#if PLIBSYS_SIZEOF_VOID_P == 8
+#define PLIBSYS_SIZEOF_LONG 8
+#else
+#define PLIBSYS_SIZEOF_LONG 4
+#endif
+#endif
+
+/* Define byte order */
+#ifdef PLIBSYS_IS_BIGENDIAN
+#define P_BYTE_ORDER P_BIG_ENDIAN
+#else
+#define P_BYTE_ORDER P_LITTLE_ENDIAN
 #endif
 
 P_END_DECLS
